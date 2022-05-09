7850b1ea7539ab1dd61bdec8cafd0fbf3e858e43
ozumin <mizuo.n@gmail.com>
2022-05-10 00:13:04 +0900

    add CardView

--- AnimationLikeAppStore/ContentView.swift
+++ AnimationLikeAppStore/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        ScrollView {
+            VStack(spacing: 30) {
+                Spacer()
+
+                Text("Today")
+                    .font(.title)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+
+                CardView()
+            }
+        }
+    }
+}
+
+struct CardView: View {
+
+    private var width: CGFloat {
+        UIScreen.main.bounds.width - 10
+    }
+
+    private var height: CGFloat {
+        300
+    }
+
+    var body: some View {
+        VStack {
+            Image(systemName: "star")
+                .frame(maxWidth: .infinity)
+
+            Text("test")
+                .frame(maxWidth: .infinity)
+        }
+        .frame(width: width, height: height)
+        .background(Color.blue)
+        .cornerRadius(20)
     }
 }
 
