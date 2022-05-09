db68fde59f5d6904af092b550019cb0f03f1899e
ozumin <mizuo.n@gmail.com>
2022-05-10 00:00:22 +0900

    init

---
+++ AnimationLikeAppStore/AnimationLikeAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationLikeAppStoreApp.swift
+//  AnimationLikeAppStore
+//
+//  Created by Mizuo Nagayama on 2022/05/09.
+//
+
+import SwiftUI
+
+@main
+struct AnimationLikeAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationLikeAppStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimationLikeAppStore
+//
+//  Created by Mizuo Nagayama on 2022/05/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
